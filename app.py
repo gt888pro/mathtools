4e6283a28c8079a70ff691ca64e72428ea47383e
lqijian <lqijian@system76-pc.localdomain>
2024-06-23 18:20:59 -0500

    test2

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import json
 import streamlit as st
 from streamlit_lottie import st_lottie
 from streamlit_extras.let_it_rain import rain
+import yfinance as yf
+import pandas as pd
+
 
 # Directories and file paths
 THIS_DIR = Path(__file__).parent
@@ -48,10 +51,42 @@ st.header(f"Welcome to my domain!", anchor=False)
 lottie_animation = load_lottie_animation(LOTTIE_CUBE_ANIMATION)
 st_lottie(lottie_animation, key="lottie-cube", height=300)
 
+
+
+
+
 # Personalized holiday message
 st.markdown(
     f"Test Website 🌟"
 )
+
+
+st.write("""
+# Simple Stock Price App
+
+Shown are the stock closing price and volume of Google!
+
+""")
+
+# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
+#define the ticker symbol
+tickerSymbol = 'GOOGL'
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2024-5-31')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
+
+
+
+
+
+
+
+
 
 
     
